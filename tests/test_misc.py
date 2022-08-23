f5a7c5ac9df4a8a97f9988841b6c10fa8a843d46
Shivam Sharma <ssshivamsharma02@gmail.com>
2022-08-23 21:39:32 +0530

    ci/cd

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -3,7 +3,7 @@ from .database import client, session
 def test_root(client):
     res = client.get("/")
     print(res.json().get('message'))
-    assert res.json().get('message') == "Hello World! successfully deployed on heroku using github-action!!"
+    assert res.json().get('message') == "Hello World! successfully deployed on heroku using github-action!"
     assert res.status_code == 200
 
 
